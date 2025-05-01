3d3adaf6900a8e5f8e031043373ee15388fdfe07
Jimmybasha <gemiahmed2002@gmail.com>
2025-05-01 09:49:59 +0300

    +Added The Models +Added The DataEnum For Movie categories +Added The Database Seeding in Program and the Seeding Function +Fixed Some Model Classes +Added The Migration +Updated The database with the Classes +Updated The Database with the Seed

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using eTickets.Data.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double Price { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public MovieCategory MovieCategory { get; set; }
+
+        //RelationShips
+        public List<Actor_Movie> MovieActors { get; set; }
+        
+        //Cinema
+        [ForeignKey("CinemaId")]
+        public int CinemaId { get; set; }
+        public Cinema Cinema { get; set; }
+
+        //Producer
+        [ForeignKey("ProducerId")]
+        public int ProducerId { get; set; }
+        public Producer Producer { get; set; }
+
+    }
+}
